eac4063d7b00143c53c59786f94a4d2a1c3c2190
Davide Gironi <davide.gironi@gmail.com>
2016-09-17 13:17:02 +0200

    updated _DevTools

--- _DevTools/git-commitandpushtomaster.bat
+++ _DevTools/git-commitandpushtomaster.bat
@@ -3,13 +3,9 @@
 rem Commit and push to master
 rem Copyright (c) Davide Gironi, 2015
 
-rem load config
-if not exist config.git-commitandpushtomaster.bat exit
-call config.git-commitandpushtomaster.bat
-
 setlocal 
 
-cd %GITMAINFOLDER%
+cd ..
 git commit -am "updated by script"
 git push
 
